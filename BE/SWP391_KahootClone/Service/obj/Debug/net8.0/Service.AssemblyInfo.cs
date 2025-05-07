d4d326e95f4e2beb38e8bb1c7a293b14e241b9ba
NET_10_VuHuuHuy <huyvhse173535@fpt.edu.vn>
2025-05-07 21:43:59 +0700

    fix gititnore

--- BE/SWP391_KahootClone/Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
+++ BE/SWP391_KahootClone/Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dbed16a5a7b02a62d07f048313c4792acd67bbf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11621233214566ebfb5c3d1d44a1549447c74bc8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
