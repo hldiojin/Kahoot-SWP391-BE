11a128b854c79c049fe40ac628af7ade426f7566
KhacTri16 <trihoang12345z@gmail.com>
2025-05-07 20:30:48 +0700

    Update stage

--- BE/SWP391_KahootClone/Repository/obj/Release/net8.0/Repository.AssemblyInfo.cs
+++ BE/SWP391_KahootClone/Repository/obj/Release/net8.0/Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dbed16a5a7b02a62d07f048313c4792acd67bbf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fd21a6cecbbf7d07895f95260fb00bc0ed420b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
